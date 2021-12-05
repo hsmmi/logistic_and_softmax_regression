903014daf2ec79c22c02130a9e4319a3767b8a51
Hesam Mousavi <hesoyam@Hesams-MacBook-Pro.local>
2021-12-05 21:06:35 +0330

    add comment

--- logistic_regression.py
+++ logistic_regression.py
@@ -22,8 +22,8 @@ def predict_logistic_regression(X_input, theta):
     """
     P = probability(X_input, theta)
     number_of_half = np.sum([P==0.5])
-    random_lable = np.random.randint(0,2,size=number_of_half)
-    P[P == 0.5] = random_lable
+    random_label = np.random.randint(0,2,size=number_of_half)
+    P[P == 0.5] = random_label
     P = (P > 0.5)
     P = P.astype(int)
     return P
@@ -35,9 +35,16 @@ def MSE(X_input ,y_input, theta):
     return ((sigmoid(X_input @ theta) - y_input).T@ \
         (sigmoid(X_input @ theta) - y_input))[0][0] / len(X_input)
 
+def MSE_prediction(predict_BC_input, y_input):
+    """
+    Gets prediction values and labels
+    Return mean square error
+    """
+    return sum((predict_BC_input - y_input)**2)[0] / len(y_input)
+
 def accuracy(predict_input ,y_input):
     """
-    Gets matrix X_input, vector y_input, and vector theta then compute 
+    Gets vector predicted value and vector y_input theta then compute 
     the percentage of the sample that was predicted correctly
     """
     return sum(predict_input == y_input)[0] / len(predict_input) * 100
@@ -75,7 +82,6 @@ def gradient_ascent_for_maximum_likelihood(X_input ,y_input, alpha, printer=0, p
 
     for i in range(1,max_epoch):
         theta = update_theta(X_input ,y_input, theta, normal_alpha * step_decay(i,10))
-        # theta = update_theta(X_input ,y_input, theta, normal_alpha)
         epoch += 1
         MSE_log.append(MSE(X_input ,y_input, theta))
         if(i > 1 and abs(MSE_log[-2]-MSE_log[-1]) < eps):
@@ -95,84 +101,20 @@ def gradient_ascent_for_maximum_likelihood(X_input ,y_input, alpha, printer=0, p
     
     return theta
 
-def logistic_regression(X_train, y_train, alpha = None, printer = 0, plotter = 0):
+def pick_one_vs_all(y_input, class_label):
     """
-    It gets matrix samples train(X_train) and their labels(y_train) and method.
-    alpha:
-    .   If alpha be None it will use closed_form if not it use gradient_descent
-        with your alpha
-    Return learned parameters (θ0 , θ1 , ..., θn ) and the value of MSE 
-    error on the train data.
+    Gets labels(y_input) and class label(class_label) that we want to select 
+    Return labels which our class label is 1 and other classes are 0
     """
-    if(printer):
-        print(f'matrix X_train is\n{X_train}\n')
-    if(printer):
-        print(f'vector y_train is\n{y_train}\n')
-
-
-    theta = gradient_ascent_for_maximum_likelihood(X_train,y_train,alpha,printer,plotter)
-
-    if(printer):
-        print(f'vector learned parameters (θ0 , θ1 , ..., θn ) is\n{theta}\n')
-
-    prediction_train = X_train @ theta
-    if(printer):
-        print(f'vector prediction_train is\n{prediction_train}\n')
-
-    error_train = prediction_train - y_train
-    if(printer):
-        print(f'vector error_train is\n{error_train}\n')
-
-    MSE_train = (np.square(error_train)).mean(axis=0)
-    if(printer):
-        print(f'MSE on train data is\n{MSE_train}\n')
-    
-    return theta, MSE_train
-
-def logistic_regression_evaluation(X_test, y_test, theta, printer = 0, plotter=0):
-    """
-    It gets matrix samples test(X_test) and their labels(y_test) and learned
-    parameters(theta) and plotter(by default 0).
-    If plotter be 1 then it'll plot the test sample and a regression line
-    Return the value of MSE error on the test data.
-    """
-    if(printer):
-        print(f'matrix X_test is\n{X_test}\n')
-    if(printer):
-        print(f'vector y_test is\n{y_test}\n')
-
-    prediction_test = X_test @ theta
-    # predict
-    if(printer):
-        print(f'vector prediction_test is\n{prediction_test}\n')
-
-    error_test = prediction_test - y_test
-    if(printer):
-        print(f'vector error_test is\n{error_test}\n')
-
-    MSE_test = np.square(error_test).mean(axis=0)
-    if(printer):
-        print(f'MSE on test data is\n{MSE_test}\n')
-
-    if(plotter):
-        sX_test = X_test.argmin(axis=0)[1]
-        eX_test = X_test.argmax(axis=0)[1]
-
-        plt.plot(list(zip(*X_test))[1], y_test, ".", label="sample")
-        plt.plot([X_test[sX_test][1],X_test[eX_test][1]], \
-            [prediction_test[sX_test],prediction_test[eX_test]], "-r", \
-                label="regression line")
-        plt.legend(loc="upper left")
-        plt.xlabel('Feature')
-        plt.ylabel('Lable')
-        plt.show()
-
-    return MSE_test
-
-def pick_one_vs_all(y_input, class_lable):
-    return np.array(y_input == class_lable, dtype=float).reshape(-1,1)
+    return np.array(y_input == class_label, dtype=float).reshape(-1,1)
 
 def one_vs_rest(X_train, X_test, y_train, y_test, alpha):
+    """
+    Gets matrix X_train, X_test, vector y_trarin, y_test, and alpha 
+    then generate all N model one_vs_all and find maximum probability 
+    for each sample in all N model
+    Return train accuracy and test accurarcy
+    """
     prob_i_rest_train = []
     prob_i_rest_test = []
     for i in range(3):
@@ -187,12 +129,23 @@ def one_vs_rest(X_train, X_test, y_train, y_test, alpha):
     return train_accuracy_ova, test_accuracy_ova
 
 def pick_one_vs_one(classes_input, class_0, class_1):
+    """
+    Get classes input and class0 and class1 
+    Return matrix X contain samples of class0 and class1 
+    and vector y contain label class0 as 0 and class1 as 1
+    """
     X_return = np.concatenate((classes_input[class_0],classes_input[class_1]))
     y_return = np.concatenate((np.zeros(len(classes_input[class_0])), \
         np.zeros(len(classes_input[class_1]))+1)).reshape(-1,1)
     return X_return, y_return
 
 def one_vs_one(X_train, X_test, y_train, y_test, classes_train, alpha):
+    """
+    Gets matrix X_train, matrix X_test, vector y_trarin, vector y_test, 
+    classes_train, and alpha then generate all c(N, 2) model one_vs_one 
+    and for each sample find mode of all models predictions
+    Return train accuracy and test accurarcy
+    """
     class_i_j_train = []
     class_i_j_test = []
     for j in range(3):
