340ab7c43929154b9de06eebb35344ccfe6b520f
Hesam Mousavi <hesoyam@Hesams-MacBook-Pro.local>
2021-12-09 09:50:02 +0330

    fix fun name

--- spr_hw_02.py
+++ spr_hw_02.py
@@ -22,7 +22,7 @@ def logistic_regression_multiclass_classification():
     train_accuracy_ovo, test_accuracy_ovo = one_vs_one(X_train, X_test, y_train, y_test, classes_train, 1)[0:2]
     print(f'Accuracy in one-vs-one on train is {round(train_accuracy_ovo,2)}% and on test is {round(test_accuracy_ovo,2)}%\n')
 
-def softmax_classification():
+def softmax_classification_runner():
     X_train, X_test, y_train, y_test = train_test_split('dataset/iris.csv',train_size=0.80,normalization="z_score")[0:4]
 
     accuracy_train_softmax, accuracy_test_softmax = softmax_classification(X_train, X_test, y_train, y_test, 1, printer=0, plotter=0)[1:3]
@@ -30,5 +30,5 @@ def softmax_classification():
 
 # logistic_regresion_binary_classification() # uncomment to run logistic regresion _ binary classification
 # logistic_regression_multiclass_classification() # uncomment to run logistic regresion _ multiclass classification
-# softmax_classification() # uncomment to run softmax classification
+# softmax_classification_runner() # uncomment to run softmax classification
 
